d52a0b35f9b44d99ae858bfab8056f0006b541ab
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-06 00:36:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -43,15 +43,15 @@ class Config:
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002456565415"))
     
     # database configs
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://nier88881:8qLsalGiPAvbwKTP@cluster0.od2tq6h.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = os.environ.get("DB_NAME", "cluster0")
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     BOT_UPTIME  = time.time()
     PORT = os.environ.get("PORT", "8080")
     
-    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
+    RKN_PIC = os.environ.get("RKN_PIC", "")
     
  # Subsprice Gif & Auto Request Accept
     SURPRICE = os.environ.get("SURPRICE", "https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4 https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4 https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4 https://telegra.ph/file/293cc10710e57530404f8.mp4 https://telegra.ph/file/506898de518534ff68ba0.mp4 https://telegra.ph/file/dae0156e5f48573f016da.mp4 https://telegra.ph/file/3e2871e714f435d173b9e.mp4 https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4 https://telegra.ph/file/876edfcec678b64eac480.mp4 https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4 https://telegra.ph/file/b4834b434888de522fa49.mp4").split()
